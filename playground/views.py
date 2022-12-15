c7ba1621c565517cd7622e8d18b6669105621a3b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-15 12:16:35 +0500

    Django

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('This is my first django project.')
 
 # Create your views here.
