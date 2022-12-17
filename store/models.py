0c12be63099fff9707153d3f255ddc83f8cc82c7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-17 12:34:36 +0500

    Django Models

--- store/models.py
+++ store/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+class Collection(models.Model):
+    title=models.CharField(max_length=255)
+
 class Product(models.Model):
     title=models.CharField(max_length=255)
     description=models.TextField()
     price=models.DecimalField(max_digits=6,decimal_places=2)
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
+    collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_BROMZE='B'
@@ -23,7 +27,9 @@ class Customer(models.Model):
     birth_date=models.DateField(null=True)
     membership=models.CharField(max_length=1,choices=membership_choice,default=MEMBERSHIP_BROMZE)
 
-class order(models.Model):
+title=models.CharField(max_length=255)
+
+class Order(models.Model):
     pending_payment='P'
     complete_payment='C'
     failed_payment='F'
@@ -34,15 +40,30 @@ class order(models.Model):
     ]
     placed_at=models.DateTimeField(auto_now_add=True)
     payment_status=models.CharField(max_length=1, choices=payment_choice,default=pending_payment)
+    order=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    items=models.ForeignKey('OrderItems', on_delete=models.CASCADE)
 
-
-class address(models.Model):
+class Address(models.Model):
     street=models.CharField(max_length=255)
     city=models.CharField(max_length=255)
     Customer=models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True) 
-    
-    
     #cascade means that when we delete the address the whole record will be deleted.
+
+
+class OrderItems(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.PROTECT)
+    price=models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity=models.PositiveSmallIntegerField()
+    unit_price=models.DecimalField(max_length=6,decimal_places=3)
+
+
+class Cart(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class CartItems(models.model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveSmallIntegerField()
 
 
 
