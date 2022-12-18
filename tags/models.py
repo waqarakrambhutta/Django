3d3a7eb85f73409a64d13348c7f328fc80f49620
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-18 15:51:58 +0500

    Django

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class Tag(models.Model):
+    label= models.CharField(max_length=255)
+
+class TaggedItems(models.Model):
+    tag= models.ForeignKey(Tag,on_delete=models.CASCADE)
+
+    content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    #using this^ solution we are bound to primary key only integers. so this is not the suitable way.
+    #if we want to know the actual object that this tag is applied to, we doo so.
+    content_object=GenericForeignKey()
+
+class LikedItems(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object=GenericForeignKey
+    
+
+
+
+
