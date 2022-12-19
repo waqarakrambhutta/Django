4612cb5cd1fa04fe29922b354442353bf7380a4c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-19 17:02:57 +0500

    customizing database schema.

--- store/models.py
+++ store/models.py
@@ -11,8 +11,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title=models.CharField(max_length=255)
+    slug=models.SlugField()
     description=models.TextField()
-    price=models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
     collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
@@ -33,6 +34,13 @@ class Customer(models.Model):
     phone=models.CharField(max_length=255)
     birth_date=models.DateField(null=True)
     membership=models.CharField(max_length=1,choices=membership_choice,default=MEMBERSHIP_BROMZE)
+    
+    class Meta:
+        db_table='store_customers'
+        indexes=[
+            models.Index(fields=['last_name','first_name'])
+            #indexes are used to speed up the queries.
+        ]
 
 title=models.CharField(max_length=255)
 
